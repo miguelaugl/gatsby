2afbe0e3792f90c81a39a06bf46c6020f9ab8c8b
Miguel <miguelaugl@outlook.com>
2020-08-24 10:01:55 -0300

    feat(gatsby): configuring gatsby project

---
+++ config/metadata.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Aprendendo gatsby',
+    siteUrl: 'http://localhost:8000',
+    description: 'This is a project made to understand and learn about gatsby',
+  },
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,67 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require('./config/metadata');
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Learning Gatsby`,
+        short_name: `Gatsby`,
+        start_url: `/`,
+        background_color: `#f6f6f6`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: 'src/assets/images/icon.jpg',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `http://localhost:9000`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: `#7159c1`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`, `500`, `700`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ]
+};
